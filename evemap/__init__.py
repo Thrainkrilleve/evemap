32599bc14567fbf672e167d2167941ca5ef6c7ee
thrainkrilleve <thrainkrill@gmail.com>
2026-07-19 09:28:21 -0600

    Align app metadata and project URLs

--- evemap/__init__.py
+++ evemap/__init__.py
@@ -1,6 +1,7 @@
-"""Example plugin app for Alliance Auth."""
+"""Eve Map plugin for Alliance Auth."""
 
 # pylint: disable = invalid-name
 default_app_config = "evemap.apps.EveMapConfig"
 
+__title__ = "Eve Map"
 __version__ = "0.0.0b5"
--- evemap/apps.py
+++ evemap/apps.py
@@ -1,9 +1,9 @@
 from django.apps import AppConfig
 
-from . import __version__
+from . import __title__, __version__
 
 
 class EveMapConfig(AppConfig):
     name = "evemap"
     label = "evemap"
-    verbose_name = f"evemap v{__version__}"
+    verbose_name = f"{__title__} v{__version__}"
